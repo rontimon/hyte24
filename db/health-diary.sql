1af7185cb9e9dee4b3d0eaf0a695d49b595a7d3e
ronitimonen <ronitim@metropolia.fi>
2024-03-15 16:25:00 +0200

    julkaisu

---
+++ db/health-diary.sql
@@ -0,0 +1,116 @@
+DROP DATABASE IF EXISTS HealthDiary;
+CREATE DATABASE HealthDiary;
+USE HealthDiary;
+
+-- Create a table for users
+CREATE TABLE Users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    user_level VARCHAR(10) NOT NULL DEFAULT 'regular'
+);
+¨
+-- Create a table for diary entries
+CREATE TABLE DiaryEntries (
+    entry_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    entry_date DATE NOT NULL,
+    mood VARCHAR(50),
+    weight DECIMAL(5,2),
+    sleep_hours INT,
+    notes TEXT,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+
+-- Tehdään taulu yksilöprojektia varten
+
+CREATE TABLE TrainingDiary (
+    diary_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    entry_date DATE NOT NULL,
+    mood INT CHECK (mood BETWEEN 1 AND 10),
+    training_time DECIMAL(5,2),
+    notes TEXT,
+    goals TEXT,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+-- Tehdään taulu HEALTHMETRICS terveystulosten kirjaamiseen
+
+CREATE TABLE HealthMetrics (
+    metric_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    measurement_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    blood_pressure_systolic INT,
+    blood_pressure_diastolic INT,
+    heart_rate INT,
+    blood_sugar DECIMAL(5,2),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+-- Tehdään taulu MEDICATIONINTAKE lääkkeiden käytön kirjaamiseen
+CREATE TABLE MedicationIntake (
+    intake_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    intake_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    medication_name VARCHAR(100),
+    dosage VARCHAR(50),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+
+
+-- INSERT User sample data
+-- Iserting multiple user rows at once
+INSERT INTO Users (username, password, email, user_level) VALUES
+  ('johndoe', 'temp-pw-1', 'johndoe@example.com', 'regular'),
+  ('janedoe', 'temp-pw-2', 'janedoe@example.com', 'admin'),
+  ('mike_smith', 'temp-pw-3', 'mike@example.com', 'moderator');
+
+
+-- Inserting multiple diary entries
+INSERT INTO DiaryEntries (user_id, entry_date, mood, weight, sleep_hours, notes, created_at) VALUES
+  (1, '2024-01-10', 'Happy', 70.5, 8, 'Had a great day, felt energetic', '2024-01-10 20:00:00'),
+  (1, '2024-01-11', 'Tired', 70.2, 6, 'Long day at work, need rest', '2024-01-11 20:00:00'),
+  (2, '2024-01-10', 'Stressed', 65.0, 7, 'Busy day, a bit stressed out', '2023-12-10 21:00:00');
+
+--   Lisätään mittaustietoja HEALTHMETRICS tauluun
+INSERT INTO HealthMetrics (user_id, measurement_date, blood_pressure_systolic, blood_pressure_diastolic, heart_rate, blood_sugar) VALUES
+    (1, '2024-01-10', 120, 80, 70, 5.4),
+    (1, '2024-01-11', 118, 79, 72, 5.5),
+    (2, '2024-01-10', 122, 82, 75, 5.3);
+
+    -- Lisätään lääkintätietoja MedicationIntake taluun
+
+    INSERT INTO MedicationInTake (user_id, intake_date, medication_name, dosage) VALUES
+(1, '2024-01-10', 'Aspirin', '100mg'),
+(1, '2024-01-11', 'Vitamin D', '5000 IU'),
+(2, '2024-01-10', 'Ibuprofen', '200mg');
+
+
+
+
+-- Example queries
+SELECT Users.username, DiaryEntries.entry_date, DiaryEntries.mood, DiaryEntries.notes
+  FROM Users, DiaryEntries
+  WHERE DiaryEntries.user_id = Users.user_id;
+
+-- Same with JOIN
+SELECT Users.username, DiaryEntries.entry_date, DiaryEntries.mood, DiaryEntries.notes
+  FROM Users JOIN DiaryEntries ON DiaryEntries.user_id = Users.user_id;
+
+-- Entries for specific username
+SELECT entry_date, mood, sleep_hours FROM DiaryEntries
+  JOIN Users ON DiaryEntries.user_id = Users.user_id
+  WHERE username = 'johndoe';
+
+-- Esimerkkisisältö TrainingDiaryyn
+
+INSERT INTO TrainingDiary (user_id, entry_date, mood, training_time, notes, goals) VALUES
+(1, '2023-03-15', 7, 1.5, 'Tuntui hyvältä juosta pitkästä aikaa. Sää oli miellyttävä.', 'Juosta maraton.'),
+(2, '2023-03-16', 8, 2.0, 'Uintiharjoitus meni erinomaisesti. Keskityin tekniikan hiomiseen.', 'Uida 1500 metriä alle 30 minuutissa.');
---
+++ src/models/entry-model.mjs
@@ -0,0 +1,250 @@
+import { param } from 'express-validator';
+import promisePool from '../utils/database.mjs';
+
+const listAllEntries = async () => {
+  try {
+    const [rows] = await promisePool.query('SELECT * FROM DiaryEntries');
+    // console.log('rows', rows);
+    return rows;
+  } catch (e) {
+    console.error('error', e.message);
+    return {error: e.message};
+  }
+};
+
+const listAllEntriesByUserId = async (id) => {
+  try {
+    const sql = 'SELECT * FROM DiaryEntries WHERE user_id=?';
+    const params = [id];
+    const [rows] = await promisePool.query(sql, params);
+    // console.log('rows', rows);
+    return rows;
+  } catch (e) {
+    console.error('error', e.message);
+    return {error: e.message};
+  }
+};
+
+const findEntryById = async (id, userId) => {
+  try {
+    const [rows] = await promisePool.query(
+      'SELECT * FROM DiaryEntries WHERE entry_id = ? AND user_id = ?',
+      [id, userId],
+    );
+    // console.log('rows', rows);
+    return rows[0];
+  } catch (e) {
+    console.error('error', e.message);
+    return {error: e.message};
+  }
+};
+
+const addEntry = async (entry, userId) => {
+  const sql = `INSERT INTO DiaryEntries
+               (user_id, entry_date, mood, weight, sleep_hours, notes)
+               VALUES (?, ?, ?, ?, ?, ?)`;
+  const params = [
+    userId,
+    entry.entry_date,
+    entry.mood,
+    entry.weight,
+    entry.sleep_hours,
+    entry.notes,
+  ];
+  try {
+    const rows = await promisePool.query(sql, params);
+    // console.log('rows', rows);
+    return {entry_id: rows[0].insertId};
+  } catch (e) {
+    console.error('error', e.message);
+    return {error: e.message};
+  }
+};
+
+const updateEntryById = async (entryId, userId, entryData) => {
+  try {
+    const params = [entryData, entryId, userId];
+    // format() function is used to include only the fields that exists
+    // in the entryData object to the SQL query
+    const sql = promisePool.format(
+      `UPDATE DiaryEntries SET ?
+       WHERE entry_id=? AND user_id=?`,
+      params,
+    );
+    const [result] = await promisePool.query(sql, params);
+    // console.log(result);
+    if (result.affectedRows === 0) {
+      return {error: 404, message: 'Entry not found'};
+    }
+    return {message: 'Entry data updated', entry_id: entryId};
+  } catch (error) {
+    // fix error handling
+    // now duplicate entry error is generic 500 error, should be fixed to 400 ?
+    console.error('updateEntryById', error);
+    return {error: 500, message: 'db error'};
+  }
+};
+
+const deleteEntryById = async (id, userId) => {
+  try {
+    const sql = 'DELETE FROM DiaryEntries WHERE entry_id=? AND user_id=?';
+    const params = [id, userId];
+    const [result] = await promisePool.query(sql, params);
+    // console.log(result);
+    if (result.affectedRows === 0) {
+      return {error: 404, message: 'Entry not found'};
+    }
+    return {message: 'Entry deleted', entry_id: id};
+  } catch (error) {
+    console.error('deleteEntryById', error);
+    return {error: 500, message: 'db error'};
+  }
+};
+
+
+// Lisätään funktiot jotka suorittavat päiväkirjaSQL kyselyt
+
+
+// Lisää päiväkirjamerkinnän:
+
+const addDiaryEntry = async (entry) => {
+  const sql = `INSERT INTO TrainingDiary (user_id, entry_date, mood, training_time, notes, goals) VALUES (?, ?, ?, ?, ?, ?)`;
+  const params = [entry.user_id, entry.entry_date, entry.mood, entry.training_time, entry.notes, entry.goals];
+  const [rows] = await promisePool.execute(sql, params);
+  return rows;
+};
+
+// Hakee päiväkirjamerkinnän UserId:n perusteella
+
+const getDiaryEntriesByUserId = async (userId) => {
+  const sql = `SELECT * FROM TrainingDiary WHERE user_id = ?`;
+  const [rows] = await promisePool.query(sql, [userId]);
+  return rows;
+};
+
+// Päivittää päiväkirjamerkinnät
+
+// const updateDiaryEntry = async (entryId, entry) => {
+//   const sql = `UPDATE TrainingDiary SET entry_date = ?, mood = ?, training_time = ?, notes = ?, goals = ? WHERE diary_id = ?`;
+//   const params = [entry.entry_date, entry.mood, entry.training_time, entry.notes, entry.goals, entryId];
+//   const [result] = await promisePool.query(sql, params);
+//   return result;
+// };
+
+const updateDiaryEntry = async (entry) => {
+  const {diary_id, entry_date, mood, training_time, notes, goals} = entry;
+  try {
+    const sql =
+      'UPDATE TrainingDiary SET entry_date=?, mood=?, training_time=?, notes=?, goals=? WHERE diary_id=?';
+    const params = [entry_date, mood, training_time, notes, goals, diary_id];
+    const [result] = await promisePool.query(sql, params);
+    // console.log(result);
+    if (result.affectedRows === 0) {
+      return {error: 404, message: 'entry not found'};
+    }
+    return {message: 'Exercise data updated', diary_id};
+  } catch (error) {
+    // fix error handling
+    // now duplicate entry error is generic 500 error, should be fixed to 400 ?
+    console.error('updateDiaryEntry', error);
+    return {error: 500, message: 'db error'};
+  }
+};
+
+// const updateDiaryEntry = async (entry) => {
+//   const sql = `UPDATE TrainingDiary SET (entry_date, mood, training_time, notes, goals) VALUES (?, ?, ?, ?, ?, ?)`;
+//   const params = [entry.entry_date, entry.mood, entry.training_time, entry.notes, entry.goals];
+//   const [rows] = await promisePool.execute(sql, params);
+//   return rows;
+// };
+
+// esimerkki
+// const updateDiaryEntry = async (entryId, entryData) => {
+//   const { entry_date, mood, training_time, notes, goals } = entryData;
+//   const sql = `
+//     UPDATE TrainingDiary
+//     SET entry_date = ?, mood = ?, training_time = ?, notes = ?, goals = ?
+//     WHERE diary_id = ?`;
+//   const params = [entry_date, mood, training_time, notes, goals, entryId];
+//   await promisePool.execute(sql, params);
+//   return { entryId, ...entryData };
+// };
+
+// Poistaa päiväkirjamerkinnät
+
+const deleteDiaryEntry = async (entryId) => {
+  const sql = `DELETE FROM TrainingDiary WHERE diary_id = ?`;
+  const [result] = await promisePool.query(sql, [entryId]);
+  return result;
+};
+
+
+// listAllDiaryEntriesByUserId
+
+// const listAllDiaryEntriesByUserId = async (id) => {
+//   try {
+//     const sql = 'SELECT * FROM TrainingDiary WHERE user_id = ?';
+//     const params = [id];
+//     const [rows] = await promisePool.query(sql, params);
+//     return rows;
+//   } catch (e) {
+//     console.error('error', e.message);
+//     return {error: e.message};
+//   }
+// };
+
+
+export {
+  listAllEntries,
+  listAllEntriesByUserId,
+  findEntryById,
+  addEntry,
+  updateEntryById,
+  deleteEntryById,
+  // Päiväkirjamerkinnät
+  addDiaryEntry,
+  getDiaryEntriesByUserId,
+  updateDiaryEntry,
+  deleteDiaryEntry,
+};
+
+//
+
+
+
+
+
+
+
+
+// // Uuden päiväkirjamerkinnän lisääminen
+// const addDiaryEntry = async (entry, userId) => {
+//   const { entry_date, mood, training_time, notes, goals } = entry;
+//   try {
+//     const [rows] = await promisePool.execute(
+//       `INSERT INTO TrainingDiary (user_id, entry_date, mood, training_time, notes, goals) VALUES (?, ?, ?, ?, ?, ?)`,
+//       [userId, entry_date, mood, training_time, notes, goals]
+//     );
+//     return { diary_id: rows.insertId };
+//   } catch (error) {
+//     console.error('addEntry error:', error.message);
+//     throw error;
+//   }
+// };
+
+// // Käyttäjän päiväkirjamerkinnän hakeminen
+
+// const listAllDiaryEntriesByUserId = async (userId) => {
+//   try {
+//     const [rows] = await promisePool.execute(
+//       `SELECT * FROM TrainingDiary WHERE user_id = ?`,
+//       [userId]
+//     );
+//     return rows;
+//   } catch (error) {
+//     console.error('listAllEntriesByUserId error:', error.message);
+//     throw error;
+//   }
+// };
+
+
